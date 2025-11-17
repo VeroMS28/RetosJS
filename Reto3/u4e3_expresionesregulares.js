4203e60774b9c84b50243f470e66d0b8017ec71d
VeroMs28 <vmarser029@g.educaand.es>
2025-11-17 18:58:36 +0100

    Reto 3 sin acabar

---
+++ Reto3/u4e3_expresionesregulares.js
@@ -0,0 +1,17 @@
+
+//Primero de todo tengo en cuenta el formulario que he realizado en el HTML
+const form = document.getElementById("formulario");
+
+//Ahora hago referencia a cada input y a los span de error que he puesto también en el HTML
+const fechaHora = document.getElementById("fechaHoraCreacion");
+const fechaHoraError = document.getElementById("errorFechaHoraCreacion");
+const cocinero = document.getElementById("cocinero");
+const cocineroError = document.getElementById("errorCocinero");
+const destinatario = document.getElementById("destinatario");
+const destinatarioError = document.getElementById("errorDestinatario");
+const gramos = document.getElementById("gramos");
+const gramosError = document.getElementById("errorGramos");
+const composicion = document.getElementById("composicion");
+const composicionError = document.getElementById("errorComposicion");
+const cuenta = document.getElementById("cuenta");
+
